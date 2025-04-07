72a891dc8fe5296d4cc0b694339a35668e2d758c
Nipuna Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2025-04-07 11:58:05 +0530

    fix bag

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'attendance_system');
+define('DB_NAME', 'if0_38670627_attendance_system');
 
 // Create database connection
 try {
--- includes/header.php
+++ includes/header.php
@@ -50,24 +50,11 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
-                    <?php if(isset($_SESSION['user_id'])): ?>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
-                                <i class="fas fa-user"></i> <?php echo htmlspecialchars($_SESSION['username']); ?>
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li><a class="dropdown-item" href="profile.php"><i class="fas fa-user-circle"></i> Profile</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-                            </ul>
-                        </li>
-                    <?php else: ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.php">
-                                <i class="fas fa-sign-in-alt"></i> Login
-                            </a>
-                        </li>
-                    <?php endif; ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">
+                            <i class="fas fa-sign-out-alt"></i> Logout
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
